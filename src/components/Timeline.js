f11361e8728434ffafd5e2821576d8553e9f7813
Leohh <948750310@qq.com>
2022-09-22 00:50:46 +0800

    feat: Daily statistics

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,12 +1,22 @@
-import { Box, Divider, Stack, Typography } from "@mui/material";
+import { Alert, AlertTitle, Box, Divider, Rating, Stack, Typography } from "@mui/material";
 import React from "react";
-import { formatDate, formatDuration, selectCommentColor, timeOfDate } from "../common/Utils";
+import {
+  formatDate,
+  formatDuration,
+  formatMilliseconds,
+  selectCommentColor,
+  timeOfDate,
+} from "../common/Utils";
 // import RadioButtonCheckedIcon from "@mui/icons-material/RadioButtonChecked";
 // import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import TaskAltIcon from "@mui/icons-material/TaskAlt";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 // import TrackChangesIcon from "@mui/icons-material/TrackChanges";
 import SlowMotionVideoIcon from "@mui/icons-material/SlowMotionVideo";
+// import PendingActionsIcon from "@mui/icons-material/PendingActions";
+import AutoFixHighIcon from "@mui/icons-material/AutoFixHigh";
+// import LightIcon from "@mui/icons-material/Light";
+// import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
 
 class Timeline extends React.Component {
   constructor(props) {
@@ -15,7 +25,7 @@ class Timeline extends React.Component {
   }
 
   momentOfObj(o) {
-    switch (o.type) {
+    switch (o.component) {
       case "stage":
         return o.actual;
 
@@ -33,19 +43,47 @@ class Timeline extends React.Component {
   get procObjs() {
     let res = [];
     let timestamp = 0;
+    let studyTaskSum = 0;
     for (const o of this.props.objs) {
       let t = timeOfDate(new Date(this.momentOfObj(o)));
       if (t !== timestamp) {
+        if (timestamp !== 0) {
+          res.push({ component: "summary", taskSum: studyTaskSum });
+          studyTaskSum = 0;
+        }
         timestamp = t;
-        res.push({ type: "divider", text: formatDate(new Date(t), "YYYY/mm/dd") });
+        res.push({ component: "divider", text: formatDate(new Date(t), "YYYY/mm/dd") });
+      }
+      if (o.component === "task" && o.type === 1 && o.actual !== null) {
+        studyTaskSum += new Date(o.tail) - new Date(o.head);
       }
       res.push(o);
     }
     return res;
   }
 
+  calcRating(ms) {
+    let hour = ms / (1000 * 60 * 60);
+    if (hour >= 12) {
+      return 5;
+    }
+    if (hour >= 10) {
+      return 4;
+    }
+    if (hour >= 8) {
+      return 3;
+    }
+    if (hour >= 6) {
+      return 2;
+    }
+    if (hour >= 4) {
+      return 1;
+    }
+    return 0;
+  }
+
   renderObj(o) {
-    switch (o.type) {
+    switch (o.component) {
       case "stage":
         return (
           <Box sx={{ borderLeft: "3px solid grey" }} pl={1}>
@@ -109,6 +147,18 @@ class Timeline extends React.Component {
               </Stack>
             )}
           </Box>
+        );
+
+      case "summary":
+        return (
+          <Alert icon={<AutoFixHighIcon fontSize="inherit" />} severity="info">
+            <AlertTitle>今日总结</AlertTitle>
+            <Typography>净学习时长：{formatMilliseconds(o.taskSum)}</Typography>
+            <Stack direction="row" justifyContent="flex-start" alignItems="center">
+              <Typography>评级：</Typography>
+              <Rating size="small" value={this.calcRating(o.taskSum)} readOnly />
+            </Stack>
+          </Alert>
         );
 
       case "divider":
